09c70b733dc4cbb2bcbb76090bc8f62365442d34
EF <1245146940@qq.com>
2020-09-20 11:23:23 +0800

    用mp01整理好的相关文件覆盖mp02中的

--- mp02/src/main/java/com/atguigu/mp/beans/Employee.java
+++ mp02/src/main/java/com/atguigu/mp/beans/Employee.java
@@ -8,17 +8,18 @@ import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
 
-/*
- * 已经全局配置了表前缀，MybatisPlus会默认使用实体类的类名到数据中找对应的表可以成功
- * 
- */
+//已经配置了全局表前缀，MybatisPlus默认使用实体类的类名到数据库中找对应的表没问题
 public class Employee extends Model<Employee> {
 	
+	//这里继承Model，已经在实现方法pkVal中，指明主键属性，
+	//不用再使用注解@TableId(value="id" , type =IdType.AUTO)
 	private Integer id ;   //  int 
 	private String  lastName; 
 	private String  email ;
+	//性别：0代表男，1代表女
 	private Integer gender; 
 	private Integer age ;
+	//数据库中无相应字段对应，可把该属性设置为不映射数据库字段
 	@TableField(exist=false)
 	private Double salary ; 
 	
--- mp02/src/main/java/com/atguigu/mp/mapper/EmployeeMapper.java
+++ mp02/src/main/java/com/atguigu/mp/mapper/EmployeeMapper.java
@@ -6,13 +6,16 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
 /**
  * Mapper接口
  * 	
- * 基于Mybatis:  在Mapper接口中编写CRUD相关的方法  提供Mapper接口所对应的SQL映射文件 以及 方法对应的SQL语句. 
+ * 基于Mybatis：在Mapper接口中编写CRUD相关的方法、提供Mapper接口所对应的SQL映射文件以及方法对应的SQL语句. 
  * 
- * 基于MP:  让XxxMapper接口继承 BaseMapper接口即可.
- * 		   BaseMapper<T> : 泛型指定的就是当前Mapper接口所操作的实体类类型 
- * 
+ * 基于MP：让XxxMapper接口继承 BaseMapper接口即可.
+ * 	BaseMapper<T>: 泛型指定的就是当前Mapper接口所操作的实体类类型 
  */
 public interface EmployeeMapper extends BaseMapper<Employee> {
+	/**
+	 * Mybatis的插入方法要实现主键自增，需要在SQL语句中添加：
+	 * 	主键自增useGeneratedKeys以及keyProperty（指定一个属性来接收主键值）属性来设置。
+	 */
     //   Integer  insertEmployee(Employee employee );
-	//   <insert useGeneratedKeys="true" keyProperty="id" > SQL...</insert>
+	//   <insert useGeneratedKeys="true" keyProperty="id" > SQL语句...</insert>
 }
