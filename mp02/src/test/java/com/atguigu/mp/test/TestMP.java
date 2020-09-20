82f85b47a2ef75966b3854b53cd092eea0507197
EF <1245146940@qq.com>
2020-09-20 13:17:04 +0800

    mp02暂时结束

--- mp02/src/test/java/com/atguigu/mp/test/TestMP.java
+++ mp02/src/test/java/com/atguigu/mp/test/TestMP.java
@@ -18,19 +18,21 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 
 public class TestMP {
+	
 	private ApplicationContext ioc = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 	
 	/**
-	 * AR  分页复杂操作
+	 * AR  分页复杂查询操作
 	 */
 	@Test
 	public void  testARPage() {
 		
 		Employee employee = new Employee();
 		
-		Page<Employee> page = employee.selectPage(new Page<>(1, 1), 
+		Page<Employee> page = employee.selectPage(new Page<>(1, 3), 
 				new EntityWrapper<Employee>().like("last_name", "老"));
+		//得到分页结果
 		List<Employee> emps = page.getRecords();
 		System.out.println(emps);
 	}
@@ -45,14 +47,17 @@ public class TestMP {
 	public void testARDelete() {
 		
 		Employee employee = new Employee();
+		
+		//根据id删除
 		//boolean result = employee.deleteById(2);
+		//等价
 //		employee.setId(2);
 //		boolean result = employee.deleteById();
 //		System.out.println("result:" +result );
 		
-		
-		boolean result = employee.delete(new EntityWrapper<Employee>().like("last_name", "小"));
-		System.out.println(result );
+		//条件构造器 模糊删除
+//		boolean result = employee.delete(new EntityWrapper<Employee>().like("last_name", "小"));
+//		System.out.println(result );
 	}
 	
 	
@@ -61,25 +66,28 @@ public class TestMP {
 	 */
 	@Test
 	public void testARSelect() {
+		
 		Employee employee = new Employee();
 		
-		//Employee result = employee.selectById(14);
-//		employee.setId(14);
+		//根据id查询
+//		Employee result = employee.selectById(6);
+		//等价：
+//		employee.setId(6);
 //		Employee result = employee.selectById();
 //		System.out.println(result );
 		
-		
+		//查询全部
 //		List<Employee> emps = employee.selectAll();
 //		System.out.println(emps);
 		
-//		List<Employee > emps= 
-//				employee.selectList(new EntityWrapper<Employee>().like("last_name", "老师"));
+		//条件构造器 模糊查询
+//		List<Employee > emps= employee.selectList(new EntityWrapper<Employee>()
+//				.like("last_name", "老师"));
 //		System.out.println(emps);
 		
-		Integer result = employee.selectCount(new EntityWrapper<Employee>().eq("gender", 0));
-		System.out.println("result: " +result );
-		
-		
+		//条件构造器 统计查询
+//		Integer result = employee.selectCount(new EntityWrapper<Employee>().eq("gender", 0));
+//		System.out.println("result: " +result );
 		
 	}
 	
@@ -89,19 +97,18 @@ public class TestMP {
 	 */
 	@Test
 	public void testARUpdate() {
-		Employee employee = new Employee();
-		employee.setId(20);
-		employee.setLastName("宋老湿");
-		employee.setEmail("sls@atguigu.com");
-		employee.setGender(1);
-		employee.setAge(36);
 		
-		
-		boolean result = employee.updateById();
-		System.out.println("result:" +result );
-		
+//		Employee employee = new Employee();
+//		employee.setId(9);
+//		employee.setLastName("宋老湿");
+//		employee.setEmail("sls@atguigu.com");
+//		employee.setGender(1);
+//		employee.setAge(35);
+//		
+//		
+//		boolean result = employee.updateById();
+//		System.out.println("result:" +result );
 	}
-	
 	
 	
 	/**
@@ -109,17 +116,14 @@ public class TestMP {
 	 */
 	@Test
 	public void  testARInsert() {
-		Employee employee = new Employee();
-		employee.setLastName("宋老师");
-		employee.setEmail("sls@atguigu.com");
-		employee.setGender(1);
-		employee.setAge(35);
 		
-		boolean result = employee.insert();
-		System.out.println("result:" +result );
+//		Employee employee = new Employee();
+//		employee.setLastName("宋老师");
+//		employee.setEmail("sls@atguigu.com");
+//		employee.setGender(1);
+//		employee.setAge(35);
+//		
+//		boolean result = employee.insert();
+//		System.out.println("result:" +result );
 	}
-	
-	
-	
-	
 }
