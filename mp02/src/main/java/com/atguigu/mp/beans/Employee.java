a4210035471964ecca003ff6135b00aa0bd5a3fe
EF <1245146940@qq.com>
2020-09-20 10:55:03 +0800

    在mp02工程中进行了简单的格式修改

--- mp02/src/main/java/com/atguigu/mp/beans/Employee.java
+++ mp02/src/main/java/com/atguigu/mp/beans/Employee.java
@@ -8,71 +8,19 @@ import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
 
-/**
- * javaBean
- * 
- * 定义JavaBean中成员变量时所使用的类型: 
- * 	 因为每个基本类型都有一个默认值:  
- * 	   int ==> 0
- * 	   boolean ==> false
- *
- */
-
 /*
- * MybatisPlus会默认使用实体类的类名到数据中找对应的表. 
+ * 已经全局配置了表前缀，MybatisPlus会默认使用实体类的类名到数据中找对应的表可以成功
  * 
  */
 public class Employee extends Model<Employee> {
+	
 	private Integer id ;   //  int 
 	private String  lastName; 
 	private String  email ;
 	private Integer gender; 
 	private Integer age ;
-	
 	@TableField(exist=false)
 	private Double salary ; 
-	
-	public Double getSalary() {
-		return salary;
-	}
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Integer getGender() {
-		return gender;
-	}
-	public void setGender(Integer gender) {
-		this.gender = gender;
-	}
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", lastName=" + lastName + ", email=" + email + ", gender=" + gender + ", age="
-				+ age + "]";
-	}
 	
 	/**
 	 * 指定当前实体类的主键属性
@@ -80,6 +28,59 @@ public class Employee extends Model<Employee> {
 	@Override
 	protected Serializable pkVal() {
 		return id;
-	} 
+	}
+	
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Integer getGender() {
+		return gender;
+	}
+	
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	
+	public Integer getAge() {
+		return age;
+	}
+	
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", lastName=" + lastName + ", email=" + email + ", gender=" + gender + ", age="
+				+ age + "]";
+	}
 	
 }
