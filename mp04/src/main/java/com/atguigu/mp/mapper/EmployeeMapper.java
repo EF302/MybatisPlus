f9061bf672770835987f9563cc34cc870582a5e8
longwanli <1245146940@qq.com>
2020-10-11 21:08:21 +0800

    myBatisPlus的各种插件了解，暂时告一段落

---
+++ mp04/src/main/java/com/atguigu/mp/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.mp.mapper;
+
+import com.atguigu.mp.beans.Employee;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author longwanli
+ * @since 2020-10-09
+ */
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+}
